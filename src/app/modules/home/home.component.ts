b76b62104a45e22a24b941dd8efcee1ffbfd7215
Vinicius Sacramento <viniciusjose57@gmail.com>
2025-04-25 20:10:47 +0100

    bind to productsAvailableCount in Home component store

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,18 +3,31 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
+import { HomeStore } from './home.store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
+    CommonModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatGridListModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  providers: [
+    HomeStore
+  ]
 })
 export class HomeComponent {
+  
+  vm$ = this._homeStore.vm$;
 
+  constructor(
+    private _homeStore: HomeStore
+  ) {
+    
+  }
 }
--- src/app/modules/home/home.store.ts
+++ src/app/modules/home/home.store.ts
@@ -1,17 +1,30 @@
 import { Injectable } from "@angular/core";
 import { ComponentStore } from "@ngrx/component-store";
 
-export interface HomeStoreState {}
+export interface HomeStoreState {
+  productsAvailableCount: number,
+}
 
 const initialState: () => HomeStoreState = () => {
-  const state: HomeStoreState = {};
+  const state: HomeStoreState = {
+    productsAvailableCount: 0
+  };
   return state;
 };
 
 @Injectable()
 export class HomeStore extends ComponentStore<HomeStoreState> {
 
+  readonly vm$ = this.select(
+    this.state$,
+    (state) => ({
+      productsAvailableCount: state.productsAvailableCount
+    })
+  );
+
   constructor() {
     super(initialState());
+
+
   }
 }
